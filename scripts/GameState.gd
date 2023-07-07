68eb02639a88de69293d1fc3563985b5d0d04baa
Tom Craven <tom.cass.craven@gmail.com>
2023-07-07 21:54:44 +0100

    wip

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -3,4 +3,9 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
+func _add_block(position):
+	pass
 
+func _get_potential_block_placement_positions():
+	# return array of positions
+	pass
---
+++ scripts/PlacementUI.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# handle displaying next block to place
+# emit signal when placed
+
+func _ready():
+	pass # Replace with function body.
