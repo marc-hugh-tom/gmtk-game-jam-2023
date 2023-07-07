479e319b4c8dfc8ba9df4d3381059d2f26a55970
Tom Craven <tom.cass.craven@gmail.com>
2023-07-07 22:15:52 +0100

    wip

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+onready var blocks_parent = get_node("blocks")
+
+const BlockFactory = preload("res://scenes/Block.tscn")
+
 func _ready():
-	pass # Replace with function body.
+	_add_block(Vector2(200, 200))
 
 func _add_block(position):
-	pass
+	var new_block = BlockFactory.instance()
+	new_block.position = position
+	
+	blocks_parent.add_child(new_block)
 
 func _get_potential_block_placement_positions():
 	# return array of positions
