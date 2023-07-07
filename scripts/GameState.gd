c15a7266df885ae9cfb86c3dc9459b08ea1c154a
Tom Craven <tom.cass.craven@gmail.com>
2023-07-08 00:19:49 +0100

    wip

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -7,3 +7,7 @@ func _process(delta):
 	var minutes = floor(win_countdown_timer.time_left / 60)
 	var seconds = int(win_countdown_timer.time_left) % 60
 	timer_label.text = "%02d:%02d" % [minutes, seconds]
+
+
+func _on_WinCountdownTimer_timeout():
+	print("win!")
