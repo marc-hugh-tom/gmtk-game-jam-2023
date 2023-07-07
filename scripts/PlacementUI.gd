0e34237d1e1ff4a717b14bfc60368e4ec7834758
Marc Jones <marc.jones@synthace.com>
2023-07-07 22:40:43 +0100

    PlacementUI

--- scripts/PlacementUI.gd
+++ scripts/PlacementUI.gd
@@ -1,7 +1,35 @@
 extends Node2D
 
-# handle displaying next block to place
-# emit signal when placed
+signal block_placed
+
+const BLOCK = preload("res://scenes/Block.tscn")
+const BORDER_MARGIN = 60
+const HALF_SPACING = 3
+const COLUMNS = 8
+const ROWS = 10
+
+var block_size
+
+var guide_block
 
 func _ready():
-	pass # Replace with function body.
+	guide_block = BLOCK.instance()
+	guide_block.set_collision_layer_bit(0, 0)
+	guide_block.set_collision_mask_bit(0, 0)
+	block_size = (
+		guide_block.get_node("CollisionShape2D").get_shape().get_extents() +
+		Vector2.ONE * HALF_SPACING
+	) * 2
+	add_child(guide_block)
+
+func _physics_process(delta):
+	var mouse = get_local_mouse_position()
+	guide_block.set_position(Vector2(
+		clamp(round(mouse.x / block_size.x), -COLUMNS/2, COLUMNS/2) * block_size.x,
+		clamp(round(mouse.y / block_size.y), -ROWS/2, ROWS/2) * block_size.y
+	))
+
+func _input(event):
+	if event.is_action_pressed("left_click"):
+		emit_signal("block_placed", guide_block.get_global_position())
+
