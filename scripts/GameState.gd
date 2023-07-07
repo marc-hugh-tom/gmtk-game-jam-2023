85e77ed7c3695b07e00e5e915857afba57f24150
Tom Craven <tom.cass.craven@gmail.com>
2023-07-07 22:36:54 +0100

    wip

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -5,14 +5,35 @@ onready var blocks_parent = get_node("blocks")
 const BlockFactory = preload("res://scenes/Block.tscn")
 
 func _ready():
-	_add_block(Vector2(200, 200))
+	pass
+	#for position in _get_potential_block_placement_positions():
+	#	_add_block(position)
 
 func _add_block(position):
 	var new_block = BlockFactory.instance()
 	new_block.position = position
-	
 	blocks_parent.add_child(new_block)
 
 func _get_potential_block_placement_positions():
-	# return array of positions
-	pass
+	var block_width = 40 * 2
+	var block_height = 10 * 2
+
+	var left_pad = 20 + (block_width / 2)
+	var right_pad = 20 + (block_width / 2)
+	var top_pad = 20 + (block_height / 2)
+	
+	var bottom_pad = 200
+	var spacing = 1
+	var max_width = 1024
+	var max_height = 600
+	
+	var width = max_width - left_pad - right_pad
+	var height = max_height - top_pad - bottom_pad
+
+	var placements = []
+	
+	for x in range(left_pad, width, block_width + spacing):
+		for y in range(top_pad, height, block_height + spacing):
+			placements.append(Vector2(x, y))
+	
+	return placements 
