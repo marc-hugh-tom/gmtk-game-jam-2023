42fd3ecbc6551108884ca3f65215c32dfd4de999
Marc Jones <marc.jones@synthace.com>
2023-07-08 01:30:58 +0100

    Fix merge bugs

--- scripts/GameContainer.gd
+++ scripts/GameContainer.gd
@@ -7,17 +7,9 @@ onready var ball = get_node("balls/Ball")
 func _ready():
 	$PlacementUI.connect("block_placed", self, "_add_block")
 	$PlacementUI.set_block_grid($BlockGrid)
-	#for position in _get_potential_block_placement_positions():
-	#	_add_block(position)
 
 func _add_block(input_global_position):
 	block_grid.add_block_to_global_position(input_global_position)
 
 func _process(delta):
 	paddle.set_ball_position(ball.position)
-
-func get_block_at(position):
-	for block in blocks.get_children():
-		if block.position.distance_squared_to(position) < 1:
-			return block
-	return null
