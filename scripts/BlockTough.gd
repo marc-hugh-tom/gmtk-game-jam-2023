5501490cfb4dc1d87aed056ef211b2a9c6abe5cd
Tom Craven <tom.cass.craven@gmail.com>
2023-07-08 09:59:25 +0100

    wip

--- scripts/BlockTough.gd
+++ scripts/BlockTough.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 var health = 2
 
-func on_collide_with_ball():
+func on_collide_with_ball(ball, collision):
 	health -= 1
 	if health == 1:
 		$Sprite.set_texture(load("res://assets/sprites/basic_block.png"))
